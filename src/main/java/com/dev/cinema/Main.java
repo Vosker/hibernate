2ffdbca2891239ca41b87d0aaf6abf08bd30c387
vosker <nikita.vaganov16@gmail.com>
2020-10-01 12:59:45 +0300

    solution added

---
+++ src/main/java/com/dev/cinema/Main.java
@@ -0,0 +1,18 @@
+package com.dev.cinema;
+
+import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieService;
+
+public class Main {
+    private static Injector injector = Injector.getInstance("com.dev.cinema");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Fast and Furious");
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
+    }
+}
