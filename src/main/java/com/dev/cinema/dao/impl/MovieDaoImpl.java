6071091c48c7d4287e75b960f1998e01d8d48af0
vosker <nikita.vaganov16@gmail.com>
2020-10-01 13:03:55 +0300

    small corrections

--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -38,7 +38,7 @@ public class MovieDaoImpl implements MovieDao {
             Query<Movie> getAllMoviesQuery = session.createQuery("from Movie", Movie.class);
             return getAllMoviesQuery.getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't get all Movies entities", e);
+            throw new RuntimeException("Can't get all Movies from DB", e);
         }
     }
 }
