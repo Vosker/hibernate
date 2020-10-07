6298a17b0e99aa185d62d1cbc869b158a2377c94
vosker <nikita.vaganov16@gmail.com>
2020-10-07 14:18:58 +0300

    solution added

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpls.java
@@ -0,0 +1,55 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpls implements MovieSessionDao {
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> getAllAvailableSessionsQuery =
+                    session.createQuery("from MovieSession where id = :id"
+                                    + " and showTime between :start AND :end",
+                            MovieSession.class);
+            getAllAvailableSessionsQuery.setParameter("id", movieId);
+            getAllAvailableSessionsQuery.setParameter("start", date.atStartOfDay());
+            getAllAvailableSessionsQuery.setParameter("end", date.atTime(LocalTime.MAX));
+            return getAllAvailableSessionsQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find available Movie Sessions", e);
+        }
+    }
+
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't create Movie Session" + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
